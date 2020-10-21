e4e1dd8d95a647c9cbcebab6ef9d438c32c5059f
Nicolas Tanski Pavão <nicolas.tanski@gmail.com>
2020-10-21 20:17:15 -0300

    Mocks and Stubs

--- tests/Service/EncerradorTest.php
+++ tests/Service/EncerradorTest.php
@@ -7,40 +7,6 @@ use Alura\Leilao\Model\Leilao;
 use Alura\Leilao\Service\Encerrador;
 use PHPUnit\Framework\TestCase;
 
-class LeilaoDaoMock extends LeilaoDao
-{
-    private $leiloes = [];
-
-    public function salva(Leilao $leilao): void
-    {
-        $this->leiloes[] = $leilao;
-    }
-
-    public function recuperarNaoFinalizados(): array
-    {
-        return array_filter(
-            $this->leiloes,
-            function(Leilao $leilao) {
-                return !$leilao->estaFinalizado();
-            });
-    }
-
-    public function recuperarFinalizados(): array
-    {
-        return array_filter(
-            $this->leiloes,
-            function(Leilao $leilao) {
-                return $leilao->estaFinalizado();
-            }
-        );
-    }
-
-    public function atualiza(Leilao $leilao)
-    {
-
-    }
-}
-
 class EncerradorTest extends TestCase
 {
     public function testLeiloesComMaisDeUmaSemanaDevemSerEncerrados()
@@ -48,18 +14,25 @@ class EncerradorTest extends TestCase
         $fiat147 = new Leilao('Fiat 147 0KM', new \DateTimeImmutable('8 days ago'));
         $variant = new Leilao('Variant 1972 0KM', new \DateTimeImmutable('10 days ago'));
 
-        $leilaoDao = new LeilaoDaoMock();
-        $leilaoDao->salva($fiat147);
-        $leilaoDao->salva($variant);
+        $leilaoDao = $this->createMock(LeilaoDao::class);
+        // $leilaoDao = $this->getMockBuilder(LeilaoDao::class)
+        //     ->setConstructorArgs([new \PDO('sqlite::memory:')])
+        //     ->getMock();
+        $leilaoDao->method('recuperarNaoFinalizados')
+            ->willReturn([$fiat147, $variant]);
+        $leilaoDao->expects($this->exactly(2))
+            ->method('atualiza')
+            ->withConsecutive(
+                [$fiat147],
+                [$variant]
+            );
 
         $encerrador = new Encerrador($leilaoDao);
         $encerrador->encerra();
 
-        $leiloes = $leilaoDao->recuperarFinalizados();
-
+        $leiloes = [$fiat147, $variant];
         self::assertCount(2, $leiloes);
-        self::assertEquals('Fiat 147 0KM',$leiloes[0]->recuperarDescricao());
-        self::assertEquals('Variant 1972 0KM',$leiloes[1]->recuperarDescricao());
-
+        self::assertTrue($leiloes[0]->estaFinalizado());
+        self::assertTrue($leiloes[1]->estaFinalizado());
     }
 }
